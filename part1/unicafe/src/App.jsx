d4a73ec1273c8a3a55562c9b4493aee5ec26add9
Noel Andia Flores <noel.andiaf@gmail.com>
2025-01-07 21:45:17 -0400

    Creación e implementación del ejercicio 1.6 unicafe paso 1

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+
+const Header = ({head}) => {
+  return (
+    <div>
+      <h1>{head}</h1>
+    </div>
+  )  
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+    </div>
+  )  
+}
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+) 
+
+const App = () => {
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const head = 'give feedback'
+  const statistics = 'statistics'
+
+  return (
+    <div>
+      <Header head={head} />
+      <div>
+        <Button handleClick = {() => setGood(good + 1)} text = "good"/>
+        <Button handleClick = {() => setNeutral(neutral + 1)} text = "neutral"/>
+        <Button handleClick = {() => setBad(bad + 1)} text = "bad"/>
+      </div>
+      <Header head={statistics} />
+      <Total good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
+
+export default App
